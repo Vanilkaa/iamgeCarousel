0b0bfce7ee3a4b46e8878d052eb1f11e09ca031c
Vanilkaa <gamvanin@gmail.com>
2024-09-08 20:56:01 +0300

    Add dots and interval cycling

--- src/createImgCarousel.js
+++ src/createImgCarousel.js
@@ -1,20 +1,31 @@
-export default function createImgCarousel(imgCarouselContainer, prevBtn, nextBtn) {
-  const imgs = imgCarouselContainer.children;
+export default function createImgCarousel(imgCarouselContainer, navDots, prevBtn, nextBtn) {
+  const imgs = Array.from(imgCarouselContainer.children);
+  const dots = Array.from(navDots.children);
   let slideIndex = 1;
 
   function showSlides(n) {
     if (n > imgs.length) slideIndex = 1;
     if (n < 1) slideIndex = imgs.length;
-    for (let i = 0; i < imgs.length; i += 1) {
-      imgs[i].style.display = 'none';
-    }
+
+    imgs.forEach((e) => e.style.display = 'none');
+    dots.forEach((e) => e.className = e.className.replace(' active', ''));
+
     imgs[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
   }
 
   function plusSlides(n) {
     showSlides(slideIndex += n);
   }
 
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
   prevBtn.onclick = () => plusSlides(-1);
   nextBtn.onclick = () => plusSlides(1);
+
+  dots.forEach((e, i) => e.onclick = () => currentSlide(i + 1));
+
+  setInterval(() => plusSlides(1), 5000);
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import './style.css';
 import createImgCarousel from './createImgCarousel';
 
 const imgCarouselContainer = document.querySelector('.imgCarouselContainer');
+const navDots = document.querySelector('.navDots');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 
-createImgCarousel(imgCarouselContainer, prevBtn, nextBtn);
+const slideShow = createImgCarousel(imgCarouselContainer, navDots, prevBtn, nextBtn);
